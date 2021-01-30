8c7e106535442e5bbec285898f808b5280d7a336
Timothé <timothe@devertry.home>
2021-01-30 17:26:37 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="index">
+    <div>
+      <img id="logo" src="#"/>
+    </div>
+
+    <div id="dropdown">
+      <div class="drop">
+        <v-select :items="items" outlined label="items" class="drops"></v-select>
+      </div>
+
+      <div class="drop">
+        <v-select :items="items" outlined label="items" class="drops"></v-select>
+      </div>
+    </div>
+
+
+    <div id="form">
+      <div id="box-form">
+      <div class="forms">
+        <p>Search</p>
+        <v-input>What do you search</v-input>
+      </div>
+
+      <div class="forms">
+        <p>Search by Tag</p>
+        <v-input>Choice between tag</v-input>
+      </div>
+    </div>
+
+    <div class="forms">
+      <p>Most Used Tag</p>
+      <v-input disabled></v-input>
+    </div>
+      <v-btn>Search</v-btn>
+    </div>
+
+    <div id="new">
+
+
+
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
---
+++ store/index.ts
@@ -0,0 +1,42 @@
+export const state = () => ({
+  data: [
+    {
+      link_fr: 'https://unesdoc.unesco.org/ark:/48223/pf0000374440/PDF/374440fre.pdf.multi',
+      link_en: 'https://unesdoc.unesco.org/ark:/48223/pf0000372505/PDF/372505eng.pdf.multi',
+      region: 'worldwide',
+      profile_fr: ['Décideur', 'Planificateur', 'enseignant'],
+      profile_en: ['Decision maker','planner','teacher'],
+      keyword_fr: ['jeune','enseignant','management enseignant'],
+      keyword_en: ['Youth', 'teachers', 'management teacher']
+    },{
+      link_fr: 'https://learningportal.iiep.unesco.org/fr/blog/surmonter-les-obstacles-urbains-%C3%A0-une-%C3%A9ducation-de-qualit%C3%A9-et-inclusive-les-enseignements-de-l \n',
+      link_en: 'https://learningportal.iiep.unesco.org/en/blog/tackling-urban-barriers-to-quality-and-inclusive-education-lessons-from-france',
+      region: 'Europe',
+      profile_fr: ['Décideur', 'Planificateur', 'enseignant'],
+      profile_en: ['Decision maker','planner','teacher'],
+      keyword_fr: ['jeune','ville','barrière social à l\'éducation'],
+      keyword_en: ['Youth', 'cities','social barrier to education']
+    },{
+      link_fr: 'https://learningportal.iiep.unesco.org/fr/blog/surmonter-les-obstacles-urbains-%C3%A0-une-%C3%A9ducation-de-qualit%C3%A9-et-inclusive-les-enseignements-de-l \n',
+      link_en: '',
+      region: 'Africa',
+      profile_fr: [],
+      profile_en: ['Decision maker','planner','teacher'],
+      keyword_fr: [],
+      keyword_en: ['Youth', 'cities','social barrier to education']
+    },{
+      link_fr: 'https://unesdoc.unesco.org/ark:/48223/pf0000373007/PDF/373007fre.pdf.multi',
+      link_en: 'https://unesdoc.unesco.org/ark:/48223/pf0000373007/PDF/373007fre.pdf.multi',
+      region: 'Africa',
+      profile_fr: ['Décideur','Planificateur'],
+      profile_en: ['Decision maker','planner'],
+      keyword_fr: ['jeune','compétence et emploie'],
+      keyword_en: ['Youth','skill and employment']
+    }
+  ],
+
+  profile_fr : ['Décideur', 'Planificateur', 'enseignant'],
+  profile_en: ['Decision maker','planner','teacher'],
+  keyword_fr: ['jeune','enseignant','management enseignant','ville','barrière social à l\'éducation','compétence et emploie'],
+  keyword_en: ['Youth', 'teachers', 'management teacher','cities','social barrier to education','skill and employment'],
+})
